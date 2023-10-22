044fd2f6001f9a78339b99fbcc565667c9fa85c2
huwngnosleep <thanhhung8210@gmail.com>
2023-10-22 11:45:37 +0700

    init

---
+++ src/components/navbar.component.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Library Management</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/books" className="nav-link">Books</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Create Book</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Students</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create Student</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
